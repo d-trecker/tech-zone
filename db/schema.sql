7848e26b26ab51d6c2fde2e598536e490e4b34be
Dylan Trecker <79778111+d-trecker@users.noreply.github.com>
2021-06-20 23:41:39 -0700

    created connection and started serverjs

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS tech_zone;
+
+CREATE DATABASE tech_zone;
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+// turns on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Tech Zone is now live on port ${PORT}.`));
+});
