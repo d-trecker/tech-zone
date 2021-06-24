219cf13fb1e4377f238cb71cdecd57a5ba28ab05
Dylan <79778111+d-trecker@users.noreply.github.com>
2021-06-24 13:22:32 -0700

    trying again

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -29,7 +29,6 @@ router.get("/", (req, res) => {
   })
     .then((dbPostData) => {
         const posts = dbPostData.map(post=> post.get({plain: true}));
-      //pass a single post object into the homepage template data
       res.render("homepage", {
         posts,
         loggedIn: req.session.loggedIn
